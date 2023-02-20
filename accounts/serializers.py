4b4ee12d5586e54a5cd0944d9bf40e597cea7007
shahbazkhan6732 <shahbaz.khan@whiteboxtech.net>
2023-02-20 19:54:42 +0500

    hgk

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -309,23 +309,21 @@ class ENUMSerializer(serializers.ModelSerializer):
 
 
 
-class ChoicesField(serializers.Field):
-    def __init__(self, choices, **kwargs):
-        self._choices = choices
-        super(ChoicesField, self).__init__(**kwargs)
+# class ChoicesField(serializers.Field):
+#     def __init__(self, choices, **kwargs):
+#         self._choices = choices
+#         super(ChoicesField, self).__init__(**kwargs)
 
-    def to_representation(self, obj):
-        return self._choices[obj]
+#     def to_representation(self, obj):
+#         return self._choices[obj]
 
-    def to_internal_value(self, data):
-        return getattr(self._choices, data)
+#     def to_internal_value(self, data):
+#         return getattr(self._choices, data)
 
 class RoleSerializer(serializers.ModelSerializer):
     role = serializers.ChoiceField(choices=Role.ROLE_CHOICES)
     class Meta:
         model = Role
-        # def get_gender(self,obj):
-        #     return obj.get_role_display()
         fields = ('role',)
 
 
